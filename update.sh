9a2ea2cf5149920a268d145bd60b35236b5046d8
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2022-09-03 11:51:43 +0200

    test

--- update.sh
+++ update.sh
@@ -13,4 +13,4 @@ fi
 # update publications
 # go to https://www.scopus.com/authid/detail.uri?authorId=24722537100 and export .bib
 # then import into HUGO as 
-# academic import --bibtex my_publications.bib
+# academic import --bibtex scopus.bib --overwrite --normalize
