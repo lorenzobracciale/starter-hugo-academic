a6ec5216686d47c26c17ac155efd1e1dc20b0fbb
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2022-09-03 11:27:43 +0200

    pub

--- update.sh
+++ update.sh
@@ -4,18 +4,11 @@ if [ -z "$1" ]; then
   echo "You need to specify the changes to commit as an argument"
   exit 1
 else
-  git add -A
-  git commit -am "$1"
-  git push
-
   hugo
 
-  cd public
-  git add -A
+  git add public
   git commit -am "$1"
   git push
-
-  cd ..
 fi
 # update publications
 # go to https://www.scopus.com/authid/detail.uri?authorId=24722537100 and export .bib
