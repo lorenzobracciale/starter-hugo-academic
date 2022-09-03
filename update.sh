ddb4c490862ebbb8989afccd6a4172a912a4ce8e
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2022-09-03 17:37:20 +0200

    menu

--- update.sh
+++ update.sh
@@ -4,11 +4,11 @@ if [ -z "$1" ]; then
   echo "You need to specify the changes to commit as an argument"
   exit 1
 else
+  hugo
   git add . 
   git add public
   git commit -am "$1"
   git push
-  hugo
   git add . 
   cd public
   git add .
